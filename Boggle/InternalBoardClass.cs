78a2b0921389f7bc3cd6e9119618192803aa002e
Ken Griffin <kgriff88@gmail.com>
2020-04-05 01:57:34 -0400

    made it so the user can't use cells they already used to create the word

--- Boggle/InternalBoardClass.cs
+++ Boggle/InternalBoardClass.cs
@@ -34,12 +34,12 @@ namespace Boggle
             board.Add(letter);
         }
 
-        public void buildWordList(string word)
+        public void buildWordList(string word)// each word a user submits is collected in the userWords list
         {
             userWords.Add(word);
         }
 
-        public string results()
+        public string results() // the begining of the end. When the timer reaches zero this method will loop through the userWords list and find if the word is valid or not.
         {
             string resultMessage = "";
 
@@ -77,7 +77,7 @@ namespace Boggle
 
         }
 
-        public int calculateScore(string word)
+        public int calculateScore(string word) // caculartes each sussecful words score and adds it to the total score
         {
             int wordPoints;
 
@@ -116,16 +116,19 @@ namespace Boggle
 
         }
 
-        public bool firstLetter(string word)
+        public bool firstLetter(string word) // finds the first letter of the word past to this method and sends the rest of the word through another method to loop through the rest of the word. Why didn't I just
+                                             // find the first letter of the word in the next method and why does this method even exist you ask? Thats a good question.
         {
 
             string start = word.Substring(0, 1);
 
             for (int k = 0; k < board.Count; k++)
             {
+                List<int> usedCells = new List<int> { };
+
                 if (start == board[k])
-                { 
-                    if (lastMethodPlease(word, k, 1))
+                {
+                    if (lastMethodPlease(word, k, 1, usedCells))
                     {
                         return true;
                     }
@@ -134,8 +137,15 @@ namespace Boggle
             return false;
         }
 
-        public bool lastMethodPlease(string word, int placed, int position)
+        public bool lastMethodPlease(string word, int placed, int position, List<int> cells) // loops through the word passed to it to find if it exists validly on the board.
         {
+
+            if (cells.Contains(placed) == true)
+            {
+                return false;
+            }
+
+            cells.Add(placed);
 
             if (position >= word.Length)
             {
@@ -145,119 +155,119 @@ namespace Boggle
             {
                 string pieces = word.Substring(position, 1);
 
-                for (int l = 0; l < board.Count; l++)
+                for (int l = 0; l < board.Count; l++) // loops through the board looking for a valid space to put the next letter in the word
                 {
                     if (pieces == board[l])
                     {
 
                         if (placed == 0 && l == 1 || placed == 0 && l == 4 || placed == 0 && l == 5)
                         {
-                            if(lastMethodPlease(word, l, position + 1))
+                            if(lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 1 && l == 0 || placed == 1 && l == 2 || placed == 1 && l == 4 || placed == 1 && l == 5 || placed == 1 && l == 6)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 2 && l == 1 || placed == 2 && l == 3 || placed == 2 && l == 5 || placed == 2 && l == 6 || placed == 2 && l == 7)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 3 && l == 2 || placed == 3 && l == 6 || placed == 3 && l == 7)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 4 && l == 0 || placed == 4 && l == 1 || placed == 4 && l == 5 || placed == 4 && l == 8 || placed == 4 && l == 9)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 5 && l == 0 || placed == 5 && l == 1 || placed == 5 && l == 2 || placed == 5 && l == 4 || placed == 5 && l == 6 || placed == 5 && l == 8 || placed == 5 && l == 9 || placed == 5 && l == 10)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 6 && l == 1 || placed == 6 && l == 2 || placed == 6 && l == 3 || placed == 6 && l == 5 || placed == 6 && l == 7 || placed == 6 && l == 9 || placed == 6 && l == 10 || placed == 6 && l == 11)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 7 && l == 2 || placed == 7 && l == 3 || placed == 7 && l == 6 || placed == 7 && l == 10 || placed == 7 && l == 11)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 8 && l == 4 || placed == 8 && l == 5 || placed == 8 && l == 9 || placed == 8 && l == 12 || placed == 8 && l == 13)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 9 && l == 4 || placed == 9 && l == 5 || placed == 9 && l == 6 || placed == 9 && l == 8 || placed == 9 && l == 10 || placed == 9 && l == 12 || placed == 9 && l == 13 || placed == 9 && l == 14)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 10 && l == 5 || placed == 10 && l == 6 || placed == 10 && l == 7 || placed == 10 && l == 9 || placed == 10 && l == 11 || placed == 10 && l == 13 || placed == 10 && l == 14 || placed == 10 && l == 15)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 11 && l == 6 || placed == 11 && l == 7 || placed == 11 && l == 10 || placed == 11 && l == 14 || placed == 11 && l == 15)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 12 && l == 8 || placed == 12 && l == 9 || placed == 12 && l == 13)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 13 && l == 8 || placed == 13 && l == 9 || placed == 13 && l == 10 || placed == 13 && l == 12 || placed == 13 && l == 14)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 14 && l == 9 || placed == 14 && l == 10 || placed == 14 && l == 11 || placed == 14 && l == 13 || placed == 14 && l == 15)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
                         }
                         else if (placed == 15 && l == 10 || placed == 15 && l == 11 || placed == 15 && l == 14)
                         {
-                            if (lastMethodPlease(word, l, position + 1))
+                            if (lastMethodPlease(word, l, position + 1, cells))
                             {
                                 return true;
                             }
